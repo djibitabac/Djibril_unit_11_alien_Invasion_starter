ae66feeb2e8e4d0c893fd8bbf0d36ae01c2c54b7
djibitabac <djibitabac@gmail.com>
2025-04-08 12:59:44 -0400

    Added ability to fire ammo and having a limit to the amount , added a laser sound

--- ship.py
+++ ship.py
@@ -3,10 +3,11 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from alien_invasion import AlienInvasion
+    from arsenal import Arsenal
 
 class Ship:
 
-    def __init__(self, game: 'AlienInvasion') -> None:
+    def __init__(self, game: 'AlienInvasion', arsenal: 'Arsenal') -> None:
         self.game = game
         self.settings = game.settings
         self.screen = game.screen
@@ -23,10 +24,15 @@ class Ship:
         self.moving_right = False
         self.moving_left = False
         self.x = float(self.rect.x)
+        self.arsenal = arsenal
 
 
 
-    def update(self):
+    def update(self) -> None:
+        self._update_ship_movement()
+        self.arsenal.update_arsenal()
+
+    def _update_ship_movement(self):
         temp_speed = self.settings.ship_speed
         if self.moving_right and self.rect.right < self.boundaries.right:
             self.x += temp_speed
@@ -36,5 +42,10 @@ class Ship:
         self.rect.x = self.x
         
     def draw(self)-> None:
+        self.arsenal.draw()
         self.screen.blit(self.image, self.rect)
+
+
+    def fire(self) -> bool:
+        return self.arsenal.fire_bullet()
 
