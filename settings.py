19694711ff9522f1df085ea077390e7175cf2bce
djibitabac <djibitabac@gmail.com>
2025-04-13 21:30:07 -0400

    lab13_dba_2.py

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ class Settings:
         self.screen_h = 800
         self.FPS = 60
 
-        self.bg_file = Path.cwd() / 'Assets' / 'images' / 'Starbasesnow.png'
+        self.bg_file = Path.cwd() / 'Assets' / 'images' / 'space.png'
         self.ship_file = Path.cwd() / 'Assets' / 'images' / 'newspaceship.jpg'
 
         
@@ -24,7 +24,7 @@ class Settings:
 
 
 
-        self.bullet_file = Path.cwd() / 'Assets' / 'images' / 'laserBlast.png'
+        self.bullet_file = Path.cwd() / 'Assets' / 'images' / 'laser16.png'
         self.laser_sound = Path.cwd() / 'Assets' / 'sound' / 'laser.mp3'
         self.impact_sound = Path.cwd() / 'Assets' / 'sound' / 'impactSound.mp3'
         self.bullet_speed = 7
@@ -33,6 +33,7 @@ class Settings:
         self.bullet_amount = 5
 
         self.alien_file = Path.cwd() / 'Assets' / 'images' / 'enemy_4.png'
+        
 
         self.fleet_speed = 2 
         self.alien_w = 40
